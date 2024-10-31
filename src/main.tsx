7c9d8ebdab6a3c86a16a351bc095fcb264016d82
silentFellow <thegreatestintelligent+github@gmail.com>
2024-10-31 15:41:11 +0530

    refactor: set persist filter for react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,24 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
+  <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
     <App />
-  </QueryClientProvider>,
+  </PersistQueryClientProvider>,
 );
